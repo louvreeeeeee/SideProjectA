f1f4da873e3f3245f9198cf509a27570adca1006
carloHP <carlodominiquetolin@gmail.com>
2024-05-14 16:41:22 +0800

    test scripts

---
+++ test/api_request.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const image = fs.readFileSync("YOUR_IMAGE.jpg", {
+    encoding: "base64"
+});
+
+axios({
+    method: "POST",
+    url: "https://detect.roboflow.com/common-rice-pests-philippines/11",
+    params: {
+        api_key: "nckOWyg6wnD7g24gr0Bd"
+    },
+    data: image,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+.then(function(response) {
+    console.log(response.data);
+})
+.catch(function(error) {
+    console.log(error.message);
+});
+
